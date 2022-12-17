aeb9c138841fa14d4a5bab7e86ff5e1ea1495b1c
nakamotoo <mitsuhiko0820.acm@gmail.com>
2022-12-17 18:51:23 +0900

    add script

---
+++ scripts/run_asterix.sh
@@ -0,0 +1,8 @@
+
+export CUDA_VISIBLE_DEVICES=1
+
+XLA_PYTHON_CLIENT_PREALLOCATE=false python -um batch_rl.fixed_replay.train \
+  --base_dir=/tmp/batch_rl \
+  --replay_dir=$DATA_DIR/Asterix/1 \
+  --agent_name=jax_dqn \
+  --gin_files='batch_rl/fixed_replay/configs/jax_dqn.gin'
